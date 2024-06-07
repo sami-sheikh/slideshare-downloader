fd1771ef0fb038c1cd66dc90e771459aa23b37cd
sami-bytequest <samiullah.bytequests@gmail.com>
2024-06-07 20:54:44 +0530

    pages

---
+++ app/contact/page.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <section className="max-w-6xl px-5 mx-auto lg:px-0">
+      <div className="flex justify-center">
+        <div className="">
+          <h2 className="py-10 text-3xl text-center">Contact us</h2>
+          <p className="max-w-[950px] flex text-start">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+            aspernatur autem nostrum accusantium perspiciatis vero eveniet
+            adipisci cumque illum, eaque porro impedit maxime totam ut, delectus
+            error nulla? Fuga perspiciatis ad exercitationem voluptatum
+            molestias at odio distinctio libero soluta sed voluptates vero
+            explicabo reiciendis, blanditiis minima veritatis, qui enim quam
+            totam esse a voluptatem praesentium perferendis quaerat. Sint vel
+            non minima accusantium fuga! Ut dolorem voluptates perferendis
+            quidem, sed culpa aspernatur quos doloribus ab cumque rem harum
+            quaerat praesentium magnam facere quas autem maiores molestias, nemo
+            dolorum corrupti alias. Soluta, nisi. Maiores beatae quas quidem
+            totam, nihil fuga reprehenderit? Ratione.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
